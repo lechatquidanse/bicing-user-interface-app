1b2e1b79e06a49ff8e5a89acc19550dc973834d2
lechatquidanse <stephane.elmanouni@gmail.com>
2018-11-15 12:30:45 +0100

    Create definition for constante.

--- src/domain/types/stationType.js
+++ src/domain/types/stationType.js
@@ -1,11 +1,14 @@
 import { shape, number, string, oneOf } from 'prop-types';
 
+import { TYPE_ALLOWED } from 'domain/definitions/stationDefinition';
 import lastAvailabilityType from 'domain/types/lastAvailabilityType';
 
 const stationType = shape({
     id: string.isRequired,
     name: string.isRequired,
-    type: oneOf(['BIKE', 'ELECTRIC_BIKE']), // @todo put those constants somewhere
+    type: oneOf(TYPE_ALLOWED),
+    address: string,
+    addressNumber: string,
     latitude: number.isRequired,
     longitude: number.isRequired,
     zipCode: string.isRequired,

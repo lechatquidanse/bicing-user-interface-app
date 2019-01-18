c4c63948c26fff65451f533f559861bfae9f49e8
lechatquidanse <stephane.elmanouni@gmail.com>
2019-01-18 17:07:15 +0100

    remove useless template.

--- src/userInterface/react/containers/ItinerarySearch/index.jsx
+++ src/userInterface/react/containers/ItinerarySearch/index.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
 import GeoLocation from 'userInterface/react/containers/ItinerarySearch/GeoLocation';
-// import SelectWhen from 'userInterface/react/containers/ItinerarySearch/SelectWhen';
 
 const ItinerarySearch = () => <>
   <GeoLocation itineraryStep={0} />
   <GeoLocation itineraryStep={1} />
-  {/*<SelectWhen />*/}
 </>;
 
 export default ItinerarySearch;
